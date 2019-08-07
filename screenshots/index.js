f00f656f90d4b0649a64d39f67a62c14338c68c9
Anthony Ilinykh <anthonyilinykh@gmail.com>
2019-08-07 10:12:47 +0300

    do a screenshot

--- screenshots/index.js
+++ screenshots/index.js
@@ -1,25 +1,32 @@
 const phantom = require('phantom')
 const sqlite = require('sqlite')
+const fs = require('fs')
 
 async function create_snapshot(username) {
   const instance = await phantom.create();
   const page = await instance.createPage();
-  await page.on('onResourceRequested', function(requestData) {
-    console.info('Requesting', requestData.url);
-  });
+  // await page.on('onResourceRequested', function(requestData) {
+  //   console.info('Requesting', requestData.url);
+  // });
  
   const status = await page.open(`https://instagram.com/${username}/`);
 
-  page.render(`screenshots/${username}.jpg`)
+  page.render(`target/${username}.jpg`)
 
   await instance.exit();
 }
 
 (async function() {
   const db = await sqlite.open('../db.sqlite3');
-  const data = await db.all('SELECT username, filtered FROM followers')
+  const data = await db.all('SELECT username, filtered FROM followers LIMIT 100')
+  let nextTimeout = 0
+  data.forEach(el => {
+    if (fs.existsSync(`target/${el.username}.jpg`))
+      return
 
-  data.forEach( el => {
-    console.log(el)
+    nextTimeout = nextTimeout + Math.min(Math.ceil(Math.random()*10), 5)
+    setTimeout(() => {
+      create_snapshot(el.username)
+    }, nextTimeout)
   })
 }())
