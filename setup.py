24f9981dbc210ef8121b42237e16301565c215a0
Anthony Ilinykh <anthonyilinykh@gmail.com>
2019-08-25 23:20:18 +0300

    setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from os import path
+from setuptools import setup, find_packages
+from instabot.__init__ import __version__
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    required = [l.strip("\n") for l in f if l.strip("\n") and not l.startswith("#")]
+
+setup(
+    name="instabot",
+    packages=find_packages(),
+    version=__version__,
+    python_requires=">3.7.0",
+    license="MIT",
+    description="Instagram Bot",
+    # long_description="",
+    # long_description_content_type="text/markdown",
+    author="Anthony Ilinykh",
+    author_email="anthonyilinykh@gmail.com",
+    url="https://github.com/ailinykh/instabot",
+    download_url="https://github.com/ailinykh/instabot/tarball/master",
+    keywords="instagram bot",
+    install_requires=required,
+    entry_points={"console_scripts": ["instabot = instabot.__main__:main"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
