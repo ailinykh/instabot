83058c889e8574868a44eee8f1958da3f1add8b7
Anthony Ilinykh <anthonyilinykh@gmail.com>
2019-08-31 15:25:01 +0300

    Release workflow setup (#2)
    * try custom gh action
    
    * tagging test
    
    * version bump
    
    * git describe test
    
    * try this one
    
    * try this
    
    * one more try
    
    * authorization
    
    * push origin
    
    * use env variables
    
    * typo fixed
    
    * separate tag to different job
    
    * required fields added
    
    * rollback version

---
+++ tag.sh
@@ -0,0 +1,23 @@
+function version_gt() { 
+    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"
+}
+
+LAST_RELEASE=`git describe origin/master | cut -c2-`
+CURRENT_VERSION=`grep -F __version__ instabot/__init__.py | awk -F "'" '{print $2}'`
+
+echo "LAST_RELEASE ${LAST_RELEASE}"
+echo "CURRENT_VERSION ${CURRENT_VERSION}"
+
+if version_gt $CURRENT_VERSION $LAST_RELEASE
+then
+    echo "Version increased!"
+    COMMIT_MESSAGE="$(git log --no-merges -2 --pretty=%B)"
+    git config --global user.email "bot@github.com"
+    git config --global user.name "Github Bot"
+    git tag -a v$CURRENT_VERSION -m "$COMMIT_MESSAGE"
+    git remote remove origin
+    git remote add origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+    git push origin --tags
+else
+    echo "Current version not greater than release"
+fi
