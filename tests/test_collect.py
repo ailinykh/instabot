3c70b4bb85c95a46406d3dc82db94474bdc2725f
Anthony Ilinykh <anthonyilinykh@gmail.com>
2019-09-02 23:52:11 +0300

    test fixed

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -5,7 +5,7 @@ from instabot import Instabot
 def test_collect(tmp_path, mock_request_get):
     defaults = {
         'database': f'sqlite:///{tmp_path}/test_db.sqlite3',
-        'profiles': ['profile']
+        'collect': {'profiles': ['profile'], 'limit': 100}
     }
     config = ConfigManager(defaults=defaults)
     bot = Instabot(config)
